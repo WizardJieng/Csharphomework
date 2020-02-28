c3672270d94489dc0cd92f2c89576a09b4a36a49
WizardJieng <1847252712@qq,com>
2020-02-28 20:39:39 +0800

    c#work

---
+++ homework2/project1/project1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入数据:");
+            int Word = int.Parse(Console.ReadLine());         
+            if (Word == 1)
+            {
+                Console.WriteLine("1没有素数因子");
+            }
+            Factor(Word);    
+        }
+        public static void Factor(int nums)
+        {
+            int i = 2;
+            while (nums > 1)
+            {
+                if (Prime(i) && nums % i == 0)
+                {
+                    Console.WriteLine($"{i}");
+                    nums = nums / i;
+                    i = 2;
+                    continue;
+                }
+                i++;
+            }
+        }
+        public static bool Prime(int n)
+        {
+            if (n == 2)
+                return true;
+            for (int i = 2; i*i < n;i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ homework2/project2/project2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            try
+            {
+                int num = 10;  //数组长度
+                int[] array = new int[num];
+                int max, min, sum, i;
+                float avg;
+                Random example = new Random();
+                Console.WriteLine("该数组为：");
+                for (i = 0; i < num; i++)
+                {
+                    array[i] = (int)(example.Next(1,55) * (num-1))+1;  //防止数过大，控制范围
+                    Console.WriteLine($"{array[i]}");
+                }               
+                max = min = array[0];
+                sum = 0;
+                for (i = 0; i < num; i++)
+                {
+                    if (max < array[i])
+                        max = array[i];
+                    if (min > array[i])
+                        min = array[i];
+                    sum += array[i];
+                }
+                avg = (float)sum / num;
+                Console.WriteLine($"最大值：{max}" + $"\n最小值：{min}" + $"\n所有数组元素的和：{sum}" + $"\n平均值：{avg}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("运算错误");
+            }
+            
+        }
+    }
+ }
+
