729ee7c52bb0c6be794f1c21703f2def6a7ede9e
WizardJieng <1847252712@qq,com>
2020-02-28 21:07:20 +0800

    c#work

---
+++ homework2/project4/project4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[101];
+            for (int i = 2; i <= 100; i++)
+            {
+                a[i] = 1;
+            }
+            for (int i = 2; i <= 100; i++)
+            {
+                if (a[i] == 1)
+                {
+                    int j = i + 1;
+                    while (j <= 100)
+                    {
+                        if (a[j] == 1 && j % i == 0)
+                        {
+                            a[j] = 0;
+                        }
+                        j++;
+                    }
+                }
+            }
+            Console.WriteLine("2~100之间的素数为：");
+            for (int i = 2; i <= 100; i++)
+                if (a[i] == 1)
+                    Console.WriteLine($"{i}");
+        }
+    }
+}
