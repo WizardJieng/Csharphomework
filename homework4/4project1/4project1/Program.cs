ad422bd26ed5573868f731b250fb5b22c77ffa58
WizardJieng <1847252712@qq,com>
2020-03-13 20:12:09 +0800

    fouthC#work

---
+++ homework4/4project1/4project1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4project1
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> next { get; set; }
+        public T data { get; set; }
+
+        public Node(T t)
+        {
+            next = null;
+            data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> n = head; n != null; n = n.next) 
+            {
+                action(n.data);                
+            }           
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            intlist.ForEach(x => Console.WriteLine(x));
+
+            int sum = 0;
+            intlist.ForEach(delegate (int x) { sum += x; });
+            Console.WriteLine($"和为：{sum}");
+
+            int max = 0;
+            intlist.ForEach(delegate (int x) { if (max < x) max = x; });
+            Console.WriteLine($"最大值为：{max}");
+
+            int min = 0;
+            intlist.ForEach(delegate (int x) { if (min > x) min = x; });
+            Console.WriteLine($"最小值为：{min}");
+
+        }
+    }
+}
---
+++ homework4/4project2/4project2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockForm clockform = new ClockForm();
+            DateTime settime = new DateTime(2020,3,13,19,7,0);
+            while(true)
+            {
+                clockform.clock.Start(settime);
+                System.Threading.Thread.Sleep(1000);
+            }            
+        }
+    }
+    public delegate void ClockElapse(object Sender, EventTime Args);
+    public class EventTime:EventArgs
+    {
+        private DateTime nowtime;
+        public DateTime NowTime
+        {
+            get => nowtime;
+            set => nowtime = value;
+        }
+        private DateTime stoptime;
+        public DateTime StopTime
+        {
+            get => stoptime;
+            set => stoptime = value;
+        }
+    }
+    public class Clock
+    {
+        public event ClockElapse Trick;
+        public event ClockElapse Alarm;
+        
+        public void Start(DateTime TimeRing)
+        {
+            EventTime time = new EventTime();
+            time.NowTime = DateTime.Now;
+            time.StopTime = TimeRing;
+            if (time.NowTime.ToString() == time.StopTime.ToString())
+            {                
+                Alarm(this, time);              
+            }
+            Trick(this, time);
+        }
+    }
+    public class ClockForm
+    {
+        public Clock clock = new Clock();    
+        public ClockForm()
+        {
+            clock.Trick += new ClockElapse(tick);
+            clock.Alarm += new ClockElapse(alarm);
+        }
+        void tick(object Sender, EventTime Args)
+        {
+            Console.WriteLine($"现在在嘀嗒，时间为：{DateTime.Now.ToString("F") }");
+        }
+        void alarm(object Sender, EventTime Args)
+        {           
+            Console.WriteLine($"到设定时间了！");
+        }
+    }
+}
